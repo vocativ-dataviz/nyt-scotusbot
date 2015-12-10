20f612127071a8cb17c96a7b8094f2f8f1c6bc80
geraldarthur <gerald.a.rich@gmail.com>
2015-12-09 17:18:26 -0800

    set db to default for prod server

--- scotusbot/__init__.py
+++ scotusbot/__init__.py
@@ -4,4 +4,4 @@ from pymongo import MongoClient
 
 CHANNEL = os.environ.get("SCOTUSBOT_SLACK_CHANNEL", None)
 MONGODB_CLIENT = MongoClient(os.environ.get('SCOTUSBOT_MONGO_URL', 'mongodb://localhost:27017/'))
-MONGODB_DATABASE = MONGODB_CLIENT.scotusbot
+MONGODB_DATABASE = MONGODB_CLIENT.get_default_database()
